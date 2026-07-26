23dd5f161b6f260bc94545af73dbc73a88a17be0
NoorAlamSabbagh <alamrangrej787@gmail.com>
2026-07-26 23:12:26 +0530

    Task

--- Databases Practice/Redis/08_PUB_SUB/src/api.js
+++ Databases Practice/Redis/08_PUB_SUB/src/api.js
@@ -1,23 +1,19 @@
 import express from "express";
-import {emailQueue} from "./queue.js";
+import Redis from "ioredis";
 
 const app = express();
 app.use(express.json());
 
-app.post("/welcome-email", async(req, res) =>{
-    const job = emailQueue.add("send-welcome-email", {
-        to: req.body.to,
-        name: req.body.name || "Learner",
-    },
-   {
-    attempts: 3,
-    backoff:{
-        type: "exponential",
-        delay: 1000,
+const publisher = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+app.post("/notifications", async(req, res) =>{
+    const payload = {
+        title: req.body.title || "Default Title",
+        createdAt: new Date().toISOString(),
     }
-   }
-);
-res.json({message: "Welcome email job added to the queue!", jobId:job.id});
+    const receivers = await publisher.publish("notifications", JSON.stringify(payload));
+    res.json({message: `Notification sent to ${receivers} subscribers`});
+    
 })
 app.listen(3000, () =>{
     console.log('Server is running on port http://localhost:3000');
