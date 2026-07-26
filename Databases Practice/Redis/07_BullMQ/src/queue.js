3dabdec86e0884bdbc02113e8a4342c34bbc5950
NoorAlamSabbagh <alamrangrej787@gmail.com>
2026-07-26 22:00:44 +0530

    update

---
+++ Databases Practice/Redis/07_BullMQ/src/queue.js
@@ -0,0 +1,13 @@
+import { Queue, Worker } from "bullmq";
+
+const connection = {
+    host: "localhost",
+    port: 6379,
+}
+
+const emailQueue = new Queue("emails", {connection});
+ 
+module.exports ={
+    emailQueue,
+    connection
+}
